29dc8d091668ad40c88ba8084861060e6a06b3e1
Shaun Moss <shaun@astromultimedia.com>
2012-08-11 11:54:08 +0700

    Fixed issue in rgb2hex JS function

--- sites/all/modules/custom/moonmars_members/js/profile-form.js
+++ sites/all/modules/custom/moonmars_members/js/profile-form.js
@@ -62,9 +62,14 @@ $(function() {
 
 function rgb2hex(rgb) {
   rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+  if (!rgb) {
+    return '';
+  }
+
   function hex(x) {
     return ("0" + parseInt(x).toString(16)).slice(-2);
   }
+
   return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
 
