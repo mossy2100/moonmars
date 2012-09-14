98386866cc496cc06e00e3df6d8e7435e0964359
Shaun Moss <shaun@astromultimedia.com>
2012-09-14 13:03:56 +1000

    Updated autoload functions

--- sites/all/classes/core/EntitySet.class.php
+++ sites/all/classes/core/EntitySet.class.php
@@ -8,24 +8,17 @@
 class EntitySet extends StarSet {
 
   /**
-   * Add one or more entities to the set.
-   * Supports both single EntityBase objects and arrays of EntityBase objects.
+   * Add entities to the set.
    *
    * @param array $entities
    */
-  public function addMultiple($entities) {
-    // Make sure we have an array:
-    if (!is_array($entities)) {
-      trigger_error("EntitySet::addMultiple() - Parameter must be an array of EntityBase objects.", E_USER_WARNING);
-    }
-
+  public function addMultiple(array $entities) {
     // Add each entity in the array:
     foreach ($entities as $entity) {
       if (!$entity instanceof EntityBase) {
         trigger_error("EntitySet::addMultiple() - Items to add must be EntityBase objects.", E_USER_WARNING);
       }
-
-      // By using the entity id as the key, we prevent duplicates.
+      // By using the entity type and id as the key, we prevent duplicates.
       $key = $entity->entityType() . ':' . $entity->id();
       $this->items[$key] = $entity;
     }
@@ -45,19 +38,13 @@ class EntitySet extends StarSet {
    *
    * @param array $entities
    */
-  public function removeMultiple($entities) {
-    // Make sure we have an array:
-    if (!is_array($entities)) {
-      trigger_error("EntitySet::removeMultiple() - Parameter must be array of EntityBase objects.", E_USER_WARNING);
-    }
-
+  public function removeMultiple(array $entities) {
     // Remove each entity in the array:
     foreach ($entities as $entity) {
       if (!$entity instanceof EntityBase) {
         trigger_error("EntitySet::removeMultiple() - Items to remove must be EntityBase objects.", E_USER_WARNING);
       }
-
-      // By using the entity uid as the key, it's easy to find the entity to remove.
+      // By using the entity type and id as the key, it's easy to find the entity to remove.
       $key = $entity->entityType() . ':' . $entity->id();
       unset($this->entities[$key]);
     }
--- sites/default/local.settings.php
+++ sites/default/local.settings.php
@@ -10,29 +10,63 @@ require_once DRUPAL_ROOT . '/sites/all/libraries/star/php/arrays.php';
 require_once DRUPAL_ROOT . '/sites/all/libraries/star/php/drupal.php';
 
 /**
+ * Scan /sites/all/classes to find where all the classes are.
+ *
+ * @param string|null $dir
+ */
+function moonmars_scan_classes_dir($dir = NULL) {
+  global $moonmars_classes;
+
+  // Default dir:
+  if ($dir === NULL) {
+    $dir = DRUPAL_ROOT . "/sites/all/classes";
+  }
+
+  $filenames = scandir($dir);
+  foreach ($filenames as $filename) {
+    if (in_array($filename, array('.', '..'))) {
+      continue;
+    }
+
+    $path = "$dir/$filename";
+    if (is_file($path)) {
+      // Check if it's a class file:
+      if (substr($filename, -10) == '.class.php') {
+        $class_name = substr($filename, 0, strlen($filename) - 10);
+        $moonmars_classes[$class_name] = $path;
+      }
+    }
+    elseif (is_dir($path)) {
+      moonmars_scan_classes_dir($path);
+    }
+  }
+}
+
+/**
  * Custom autoload function.
  *
  * @param $class
  */
-function moonmars_autoload($class) {
-  // Try sites/all/classes:
-  $path = DRUPAL_ROOT . "/sites/all/classes/$class.class.php";
-  if (file_exists($path)) {
-    require_once $path;
-    return;
+function moonmars_autoload($class_name) {
+  // Get the location of our classes if not done already:
+  global $moonmars_classes;
+  if (!isset($moonmars_classes)) {
+    // Check the cache:
+    $moonmars_classes_cache = cache_get('moonmars_classes');
+    if ($moonmars_classes_cache) {
+      $moonmars_classes = $moonmars_classes_cache->data;
+    }
+    else {
+      // Not in cache, so scan the classes folder:
+      moonmars_scan_classes_dir();
+      // Cache that shit:
+      cache_set('moonmars_classes', $moonmars_classes, 'cache', CACHE_TEMPORARY);
+    }
   }
 
-  // Try sites/all/classes/core:
-  $path = DRUPAL_ROOT . "/sites/all/classes/core/$class.class.php";
-  if (file_exists($path)) {
-    require_once $path;
-    return;
-  }
-
-  // Try sites/all/libraries/star/php/classes:
-  $path = DRUPAL_ROOT . "/sites/all/libraries/star/php/classes/$class.class.php";
-  if (file_exists($path)) {
-    require_once $path;
+  // If we have this class, load it:
+  if (array_key_exists($class_name, $moonmars_classes)) {
+    require_once $moonmars_classes[$class_name];
     return;
   }
 }
