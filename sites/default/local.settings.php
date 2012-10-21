d94a0082a00f022b65b66dfb4e83f55c4f928498
Shaun Moss <shaun@astromultimedia.com>
2012-10-21 15:46:03 +0700

    Removed debug code

--- sites/default/local.settings.php
+++ sites/default/local.settings.php
@@ -15,39 +15,6 @@ require_once "$star_path/php/objects.php";
 require_once "$star_path/php/drupal.php";
 
 /**
- * Scan /sites/all/classes to find where all the classes are.
- *
- * @param string|null $dir
- */
-function moonmars_scan_classes_dir($dir = NULL) {
-  global $moonmars_classes;
-
-  // Default dir:
-  if ($dir === NULL) {
-    $dir = DRUPAL_ROOT . "/sites/all/classes";
-  }
-
-  $filenames = scandir($dir);
-  foreach ($filenames as $filename) {
-    if (in_array($filename, array('.', '..'))) {
-      continue;
-    }
-
-    $path = "$dir/$filename";
-    if (is_file($path)) {
-      // Check if it's a class file:
-      if (substr($filename, -10) == '.class.php') {
-        $class_name = substr($filename, 0, strlen($filename) - 10);
-        $moonmars_classes[$class_name] = $path;
-      }
-    }
-    elseif (is_dir($path)) {
-      moonmars_scan_classes_dir($path);
-    }
-  }
-}
-
-/**
  * Custom autoload function based on the PSR-0 standard, which is used in D8 and other frameworks.
  *
  * @see https://gist.github.com/1234504
@@ -71,26 +38,3 @@ function psr0_autoload($class_name) {
 
 // Register the PSR-0 autoload function:
 spl_autoload_register('psr0_autoload');
-
-// Old autoload code:
-//  // Get the location of our classes if not done already:
-//  global $moonmars_classes;
-//  if (!isset($moonmars_classes)) {
-//    // Check the cache:
-//    $moonmars_classes_cache = cache_get('moonmars_classes');
-//    if ($moonmars_classes_cache) {
-//      $moonmars_classes = $moonmars_classes_cache->data;
-//    }
-//    else {
-//      // Not in cache, so scan the classes folder:
-//      moonmars_scan_classes_dir();
-//      // Cache that shit:
-//      cache_set('moonmars_classes', $moonmars_classes, 'cache', CACHE_TEMPORARY);
-//    }
-//  }
-//
-//  // If we have this class, load it:
-//  if (array_key_exists($class_name, $moonmars_classes)) {
-//    require_once $moonmars_classes[$class_name];
-//    return;
-//  }
