fa0fa758737766420b1c68476bf2d9fc797406a8
Shaun Moss <shaun@astromultimedia.com>
2012-11-17 10:38:43 +0700

    moved nxnx for new groups to moonmars_group mod

--- sites/all/libraries/star/php/numbers.php
+++ sites/all/libraries/star/php/numbers.php
@@ -22,12 +22,10 @@ function mod($n, $d) {
 }
 
 /**
- * Handy function that PHP should have included.
+ * Handy function that returns TRUE if the param is equivalent to a signed integer.
+ * Use as an alternative to is_numeric() when you want to know if the value looks like an int, not just a number.
  *
- * "is_sint" means "is signed integer". This doesn't refer to the value having the type "integer", but whether the
- * value is equivalent to a signed integer (positive, 0 or negative).
- *
- * Returns true if param is a signed integer, or a string of float that looks like one.
+ * "is_sint" means "is signed integer". This doesn't refer to the value having the type "integer".
  *
  * @param mixed $n
  * @return bool
@@ -37,16 +35,50 @@ function is_sint($n) {
 }
 
 /**
- * Handy function that PHP should have included.
- *
- * "is_uint" means "is unsigned integer". This doesn't refer to the value having the type "integer", but whether the
- * value is equivalent to a unsigned integer (0 or greater).
- *
- * Returns true if param is an unsigned integer, or a string of float that looks like one.
+ * Handy function that returns TRUE if the param is equivalent to an unsigned integer.
+ * "is_uint" means "is unsigned integer". This doesn't refer to the value having the type "integer".
  *
  * @param mixed $n
  * @return bool
  */
 function is_uint($n) {
   return is_sint($n) && $n >= 0;
+}
+
+/**
+ * Handy function that returns TRUE if the param is equivalent to a positive integer.
+ * "is_pint" means "is positive integer". This doesn't refer to the value having the type "integer".
+ *
+ * @param mixed $n
+ * @return bool
+ */
+function is_pint($n) {
+  return is_sint($n) && $n > 0;
+}
+
+/**
+ * Handy function that returns TRUE if the param is equivalent to a negative integer.
+ * "is_nint" means "is negative integer". This doesn't refer to the value having the type "integer".
+ *
+ * @param mixed $n
+ * @return bool
+ */
+function is_nint($n) {
+  return is_sint($n) && $n < 0;
+}
+
+/**
+ * Handy function that returns TRUE if the param is equivalent to (i.e. looks like) 0.
+ *
+ * NOTE, this function is not the same as $n == 0, because it will return FALSE if the param is NULL or FALSE or '' or
+ * anything else falsy that that doesn't look like 0.
+ * This is also not the same as $n === 0, because it will return TRUE for strings or floats equivalent to 0.
+ *
+ * @todo test
+ *
+ * @param mixed $n
+ * @return bool
+ */
+function is_zero($n) {
+  return is_sint($n) && $n == 0;
 }
